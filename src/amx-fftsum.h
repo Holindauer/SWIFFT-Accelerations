81c632ad60955063d9798b56e537938248648fb5
Hunter Lindauer <holindauer@gmail.com>
2024-11-05 09:32:05 -0800

    impl: Vectorized 32x64 transpose

--- src/amx-fftsum.h
+++ src/amx-fftsum.h
@@ -2,13 +2,85 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <xmmintrin.h>  
 
 #define SWIFFT_M 32
 #define SWIFFT_N 64
 
-
 void amx_fftsum(const uint16_t fftout, int m, uint16_t out);
-
 
 // Dummy uint16_t PI key, // TODO replace later w/ actual
 extern const uint16_t PI_key_dummy[SWIFFT_M*SWIFFT_N];
+
+/* transposition of an 8x8 matrix */
+inline void transpose_8x8_16_sse2(__m128i * array)
+{
+	__m128i *p_input  = (__m128i*)array;
+	__m128i *p_output = (__m128i*)array;
+	__m128i a = _mm_load_si128(p_input++);
+	__m128i b = _mm_load_si128(p_input++);
+	__m128i c = _mm_load_si128(p_input++);
+	__m128i d = _mm_load_si128(p_input++);
+	__m128i e = _mm_load_si128(p_input++);
+	__m128i f = _mm_load_si128(p_input++);
+	__m128i g = _mm_load_si128(p_input++);
+	__m128i h = _mm_load_si128(p_input);
+	
+	__m128i a03b03 = _mm_unpacklo_epi16(a, b);
+	__m128i c03d03 = _mm_unpacklo_epi16(c, d);
+	__m128i e03f03 = _mm_unpacklo_epi16(e, f);
+	__m128i g03h03 = _mm_unpacklo_epi16(g, h);
+	__m128i a47b47 = _mm_unpackhi_epi16(a, b);
+	__m128i c47d47 = _mm_unpackhi_epi16(c, d);
+	__m128i e47f47 = _mm_unpackhi_epi16(e, f);
+	__m128i g47h47 = _mm_unpackhi_epi16(g, h);
+	
+	__m128i a01b01c01d01 = _mm_unpacklo_epi32(a03b03, c03d03);
+	__m128i a23b23c23d23 = _mm_unpackhi_epi32(a03b03, c03d03);
+	__m128i e01f01g01h01 = _mm_unpacklo_epi32(e03f03, g03h03);
+	__m128i e23f23g23h23 = _mm_unpackhi_epi32(e03f03, g03h03);
+	__m128i a45b45c45d45 = _mm_unpacklo_epi32(a47b47, c47d47);
+	__m128i a67b67c67d67 = _mm_unpackhi_epi32(a47b47, c47d47);
+	__m128i e45f45g45h45 = _mm_unpacklo_epi32(e47f47, g47h47);
+	__m128i e67f67g67h67 = _mm_unpackhi_epi32(e47f47, g47h47);
+	
+	__m128i a0b0c0d0e0f0g0h0 = _mm_unpacklo_epi64(a01b01c01d01, e01f01g01h01);
+	__m128i a1b1c1d1e1f1g1h1 = _mm_unpackhi_epi64(a01b01c01d01, e01f01g01h01);
+	__m128i a2b2c2d2e2f2g2h2 = _mm_unpacklo_epi64(a23b23c23d23, e23f23g23h23);
+	__m128i a3b3c3d3e3f3g3h3 = _mm_unpackhi_epi64(a23b23c23d23, e23f23g23h23);
+	__m128i a4b4c4d4e4f4g4h4 = _mm_unpacklo_epi64(a45b45c45d45, e45f45g45h45);
+	__m128i a5b5c5d5e5f5g5h5 = _mm_unpackhi_epi64(a45b45c45d45, e45f45g45h45);
+	__m128i a6b6c6d6e6f6g6h6 = _mm_unpacklo_epi64(a67b67c67d67, e67f67g67h67);
+	__m128i a7b7c7d7e7f7g7h7 = _mm_unpackhi_epi64(a67b67c67d67, e67f67g67h67);
+	
+	_mm_store_si128(p_output++, a0b0c0d0e0f0g0h0);
+	_mm_store_si128(p_output++, a1b1c1d1e1f1g1h1);
+	_mm_store_si128(p_output++, a2b2c2d2e2f2g2h2);
+	_mm_store_si128(p_output++, a3b3c3d3e3f3g3h3);
+	_mm_store_si128(p_output++, a4b4c4d4e4f4g4h4);
+	_mm_store_si128(p_output++, a5b5c5d5e5f5g5h5);
+	_mm_store_si128(p_output++, a6b6c6d6e6f6g6h6);
+	_mm_store_si128(p_output, a7b7c7d7e7f7g7h7);
+}
+
+
+/* Transposes a 32x64 matrix using sse2 intrinsics */
+inline void transpose_32x64_sse2(const int16_t *input, int16_t *output, int rows, int cols) {
+    for (int i = 0; i < rows; i += 8) {
+        for (int j = 0; j < cols; j += 8) {
+            __m128i block[8];
+
+            // Load 8x8 block from input
+            for (int k = 0; k < 8; ++k) {
+                block[k] = _mm_load_si128((__m128i *)&input[(i + k) * cols + j]);
+            }
+            // SIMD transposition of 8x8 int16_t transpose func 
+            transpose_8x8_16_sse2(block);
+
+            // Store the transposed block into output
+            for (int k = 0; k < 8; ++k) {
+                _mm_store_si128((__m128i *)&output[(j + k) * rows + i], block[k]);
+            }
+        }
+    }
+}
--- src/test-amx-fftsum.c
+++ src/test-amx-fftsum.c
@@ -1,9 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+
 #include "amx-fftsum.h"
 
+/* naive int16_t matrix transposition */
+static void naive_transpose_matrix_int16_t(const int16_t *input, int16_t *output, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            output[j * rows + i] = input[i * cols + j];
+        }
+    }
+}
 
+void test_transpose_32x64_sse2() {
+    
+    // init input mats
+    int16_t naive_input[SWIFFT_M*SWIFFT_N] __attribute__((aligned(16)));
+    int16_t vectorized_input[SWIFFT_M*SWIFFT_N] __attribute__((aligned(16)));
+    
+    for (int i = 0; i < 32 * 64; i++) {
+        naive_input[i] = i % 256;
+        vectorized_input[i] = i % 256;
+    }
+
+    // init output mats
+    int16_t output_naive[32 * 64] __attribute__((aligned(16)));
+    int16_t output_vectorized[32 * 64] __attribute__((aligned(16)));
+    memset(output_naive, 0, sizeof(output_naive));
+    memset(output_vectorized, 0, sizeof(output_vectorized));
+
+    // transpose using naive method
+    naive_transpose_matrix_int16_t(naive_input, output_naive, 32, 64);
+
+    // transpose using sse2
+    transpose_32x64_sse2(vectorized_input, output_vectorized, 32, 64);
+
+    // compare results
+    for (int i = 0; i < 32 * 64; i++) {
+        if (output_naive[i] != output_vectorized[i]) {
+            puts("test_transpose_32x64_sse2... FAIL!");
+            return;
+        }
+    }
+
+    puts("test_transpose_32x64_sse2... PASS!");
+}
 
 int main(void){
 
+    test_transpose_32x64_sse2();
 
     return 0;
 }
